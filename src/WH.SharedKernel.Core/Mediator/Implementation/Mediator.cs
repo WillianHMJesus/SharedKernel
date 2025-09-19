57ebf3b4ff2cca9e52d9b6a0229f52b4a3ac6aca
willian Hilton <willian.hilton@hotmail.com>
2025-09-18 23:41:16 -0300

    Implementation of the mediator class

---
+++ src/WH.SharedKernel.Core/Mediator/Implementation/Mediator.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace WH.SharedKernel.Mediator;
+
+public class Mediator(SimpleMediator.IMediator mediator) : IMediator
+{
+    public async Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default)
+        where TNotification : SimpleMediator.INotification
+    {
+        await mediator.Publish(notification, cancellationToken);
+    }
+
+    public async Task<TResponse> Send<TResponse>(SimpleMediator.IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    {
+        return await mediator.Send(request, cancellationToken);
+    }
+}
+
---
+++ src/WH.SharedKernel.Core/Mediator/Interfaces/IMediator.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WH.SimpleMediator;
+
+namespace WH.SharedKernel.Mediator;
+
+public interface IMediator
+{
+    Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+
+    Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default)
+        where TNotification : INotification;
+}
